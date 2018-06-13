a11df25ccad39fe8af562f50b785e13a52b8b4b0
rtyui <fghjkl>
2018-06-12 22:15:38 -0500

    Mi madrecita

---
+++ Proyect 3D/Assets/CamMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMovement : MonoBehaviour {
+
+    public Transform lookTarget;
+    public float targetHeight;
+    public Vector3 targetPoint;
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = Vector3.MoveTowards(transform.position, lookTarget.position + targetPoint, 5f * Time.deltaTime);
+        transform.LookAt(lookTarget);
+        
+
+        /*transform.LookAt(lookTarget);
+        float difference = Mathf.Abs (transform.position.y - lookTarget.position.y);
+        if (difference != targetHeight) {
+            Vector3 temp = transform.position;
+            temp.y = Mathf.MoveTowards(temp.y, targetHeight, 5f * Time.deltaTime);
+            transform.position = temp;
+        }
+        if (transform.position.x != lookTarget.position.x){
+            Vector3 temp = transform.position;
+            temp.x = Mathf.MoveTowards(temp.x, lookTarget, 5f * Time.deltaTime);
+            
+        }*/
+	}
+}
---
+++ Proyect 3D/Assets/PlatformerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformerMovement : MonoBehaviour
+{
+    public float horizontalSpeed;
+    Vector3 movement;
+    public Rigidbody rigidbody3D;
+    public float impulseValue;
+     // Use this for initialization
+    void Start(){
+    
+    }
+
+    // Update is called once per frame
+ void Update(){
+        movement = transform.position;
+        float horizontalDirection = Input.GetAxis("Horizontal");
+        float verticalDirection = Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.J)){
+            transform.Rotate(Vector3.up * 100f * Time.deltaTime);
+        }
+        if (horizontalDirection != 0){
+            movement += Vector3.right * horizontalDirection * horizontalSpeed * Time.deltaTime;
+        }
+        if (verticalDirection !=0){
+            movement += Vector3.forward * verticalDirection * horizontalSpeed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.Space)){
+            rigidbody3D.AddForce (Vector3.up * impulseValue, ForceMode.Impulse);
+        }
+        rigidbody3D.MovePosition(movement);
+}
+}
